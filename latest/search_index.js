34a77ac382943d885a6bc81cc4c7503679fcee66
autodocs <autodocs>
2018-12-13 21:17:53 +0000

    build based on 40d4339

--- latest/search_index.js
+++ latest/search_index.js
@@ -9,11 +9,19 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "index.html#ContextCF.jl-1",
+    "location": "index.html#ContextCF.jl-Context-Collaborative-Filtering-expansion-for-package-Persa.jl(https://github.com/JuliaRecsys/Persa.jl)-1",
     "page": "Introduction",
-    "title": "ContextCF.jl",
+    "title": "ContextCF.jl - Context Collaborative Filtering expansion for package Persa.jl(https://github.com/JuliaRecsys/Persa.jl)",
     "category": "section",
-    "text": "Documentation for ContextCF.jl"
+    "text": "Installation: at the Julia REPL, Pkg.add(\"ContextCF\") or in Pkg3 add ContextCF"
+},
+
+{
+    "location": "index.html#Goal-1",
+    "page": "Introduction",
+    "title": "Goal",
+    "category": "section",
+    "text": "The main aim is to extend the Recommender Systems framework Persa.jl(https://github.com/JuliaRecsys/Persa.jl), enabling easy study of Context Aware Recommender Systems in Julia.Pages = [     \"man/ratings.md\",     \"man/dataset.md\", ]Depth = 1"
 },
 
 ]}
